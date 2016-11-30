a876de1124dbf62ebe4b5b141e0f5204bf6937c5
Lambert Boskamp <lambert@boskamp-consulting.com>
2016-11-30 13:48:29 +0000

    Add SAP deployment descriptors; do JNDI lookup

--- servlet/src/main/java/de/foxysoft/rhidmo/Test.java
+++ servlet/src/main/java/de/foxysoft/rhidmo/Test.java
@@ -1,14 +1,36 @@
 package de.foxysoft.rhidmo;
 
+import javax.naming.InitialContext;
+
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.dynamic.DynamicType;
 
 public class Test {
-    public static void main(String[] args) throws Exception {
-        Class c = Class.forName("com.sap.idm.extension.TaskProcessingAdapter");
-        DynamicType.Unloaded<?> dynamicType = new ByteBuddy()
-            .subclass(c)
-            .name("de.foxysoft.rhidmo.TaskProcessing")
-            .make();
-    }
+	public static void main(String[] args) throws Exception {
+		if (args == null || args.length == 0
+				|| args[0].equalsIgnoreCase("jndi")) {
+			doJndi();
+		} else if (args[0].equalsIgnoreCase("subclass")) {
+			doSubclass();
+		}
+	}
+
+	private static void doSubclass() throws Exception {
+		Class c = Class.forName("com.sap.idm.extension.TaskProcessingAdapter");
+		DynamicType.Unloaded<?> dynamicType = new ByteBuddy().subclass(c)
+				.name("de.foxysoft.rhidmo.TaskProcessing").make();
+	}
+
+	private static void doJndi() throws Exception {
+		final String M = "doJndi: ";
+		InitialContext ctx = new InitialContext();
+		Object o = ctx.lookup("IDM");
+		trc(M + "o=" + o);
+
+	}
+
+	private static String trc(String msg) {
+		System.out.println(msg);
+		return msg;
+	}
 }
