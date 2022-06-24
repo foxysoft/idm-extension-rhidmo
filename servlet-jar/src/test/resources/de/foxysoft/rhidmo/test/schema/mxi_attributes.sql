fcf6a3b9bf10a832ff163fbd0140ad92b95a527c
Lambert Giese <lambert@boskamp-consulting.com>
2022-06-24 11:27:49 +0200

    Add missing license headers

--- servlet-jar/src/test/resources/de/foxysoft/rhidmo/test/schema/mxi_attributes.sql
+++ servlet-jar/src/test/resources/de/foxysoft/rhidmo/test/schema/mxi_attributes.sql
@@ -1,4 +1,19 @@
--- Surrogate definition of idmv_value_basic for unit tests based on H2 database.
+/*******************************************************************************
+ * Copyright 2022 Lambert Giese
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+-- Surrogate definition of mxi_attributes for unit tests based on H2 database.
 -- Only those columns accessed by Rhidmo are included. Datatypes are specific to H2.
 CREATE TABLE mxi_attributes (
 	AttrName varchar(200),
